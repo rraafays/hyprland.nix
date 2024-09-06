a58c78be45046aa18e719b8719900dd5ec8959c1
raf <rraf@tuta.io>
2024-09-06 20:14:47 +0100

    chore: moved auto start hyprland

--- default.nix
+++ default.nix
@@ -4,6 +4,20 @@ let
     USER = "raf";
 in
 {
+    services.greetd = {
+        enable = true;
+        settings = {
+            initial_session = {
+                command = "${pkgs.hyprland}/bin/Hyprland";
+                user = "${USER}";
+            };
+            default_session = {
+                command = "${pkgs.hyprland}/bin/Hyprland";
+                user = "${USER}";
+            };
+        };
+    };
+
   security.polkit.enable = true;
   xdg.portal = {
     enable = true;
